11750e48598973097253d49cf939f60ce02cb4c4
KoalaV2 <theo.johanson@gmail.com>
2022-09-10 16:47:33 +0200

    nvim: Cleanup

--- nvim/init.vim
+++ nvim/init.vim
@@ -5,13 +5,7 @@ source $HOME/.config/nvim/sets.vim
 colorscheme gruvbox
 let g:copilot_node_command = '/home/theo/.nvm/versions/node/v17.9.0/bin/node'
 let g:rainbow_active = 1
-let g:SuperTabDefaultCompletionType = "<c-n>"
 let g:indent_blankline_show_first_indent_level = v:false
-" let g:prettier#autoformat = 1
-" let g:prettier#autoformat_require_pragma = 0
-let g:copilot_filetypes = {
-      \ 'txt': v:false,
-      \ }
 let g:copilot_no_tab_map = v:true
 let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
 let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,7 +1,7 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
 require("mason-lspconfig").setup_handlers({
-  function (server_name) -- default handler (optional)
+function (server_name) -- default handler (optional)
     require("lspconfig")[server_name].setup {}
   end,
 })
@@ -9,18 +9,14 @@ require("mason-lspconfig").setup_handlers({
 local cmp = require'cmp'
 cmp.setup({
     snippet = {
-      -- REQUIRED - you must specify a snippet engine
       expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
       end,
     },
-    window = {
+    -- window = {
       -- completion = cmp.config.window.bordered(),
       -- documentation = cmp.config.window.bordered(),
-    },
+    -- },
     mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
@@ -30,10 +26,7 @@ cmp.setup({
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
-      { name = 'vsnip' }, -- For vsnip users.
-      -- { name = 'luasnip' }, -- For luasnip users.
-      -- { name = 'ultisnips' }, -- For ultisnips users.
-      -- { name = 'snippy' }, -- For snippy users.
+      { name = 'luasnip' },
     }, {
       { name = 'buffer' },
     })
@@ -65,16 +58,3 @@ cmp.setup({
       { name = 'cmdline' }
     })
 })
-
-
--- Set up lspconfig.
--- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
--- require('lspconfig')['pyright'].setup {
--- capabilities = capabilities
--- }
-
-
--- require('lspconfig')['pyright'].setup{}
--- require('lspconfig')['vimls'].setup{}
--- require('lspconfig')['html'].setup{}
