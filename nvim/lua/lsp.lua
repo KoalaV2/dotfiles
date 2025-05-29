37c6cf0b9fb582d8f2d7346864dbcef7a4abe721
Theodor Johanson <theo.johanson@gmail.com>
2025-05-29 17:55:40 +0200

    changes

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,10 +1,10 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
-require("mason-lspconfig").setup_handlers({
-function (server_name) -- default handler (optional)
-    require("lspconfig")[server_name].setup {}
-  end,
-})
+-- require("mason-lspconfig").setup_handlers({
+-- function (server_name) -- default handler (optional)
+--     require("lspconfig")[server_name].setup {}
+--   end,
+-- })
 
 local cmp = require'cmp'
 cmp.setup({
